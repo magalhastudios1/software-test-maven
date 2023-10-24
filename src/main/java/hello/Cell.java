b324e3df8550bb8fd878740a5b47309510bc667c
magalhastudios1 <magalhastudios1@gmail.com>
2023-10-24 19:25:49 -0300

    Update Cell.java

--- src/main/java/hello/Cell.java
+++ src/main/java/hello/Cell.java
@@ -1,11 +1,5 @@
 package hello;
 
-public class Cell{
-    private float value;
-
-    public Cell(){}
-
-    public int getValue(){
-        return this.value;
-    }
+public abstract class Cell{
+    public float getValue();
 }
