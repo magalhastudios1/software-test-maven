9c5879d206d120a746330c2024af4ea12428aeb6
magalhastudios1 <magalhastudios1@gmail.com>
2023-11-03 18:41:51 -0300

    Debugging

--- src/main/java/sheet/Sheet.java
+++ src/main/java/sheet/Sheet.java
@@ -2,15 +2,15 @@ package sheet;
 
 import cell.Cell;
 
-public final class Sheet{
+public final class Sheet {
     private static Sheet instance;
 
     private int width;
     private int height;
 
-    private Cell [][] board;
+    private Cell[][] board;
 
-    private Sheet(int width, int height){
+    private Sheet(int width, int height) {
         this.width = width;
         this.height = height;
 
@@ -24,46 +24,51 @@ public final class Sheet{
         return instance;
     }
 
-    public int getArea(){
+    public int getArea() {
         return this.width * this.height;
     }
 
-    public void addElement(Cell cell, int x, int y){
+    public void addElement(Cell cell, int x, int y) {
         board[x][y] = cell;
     }
 
-    public Cell getElement(int x, int y){
+    public Cell getElement(int x, int y) {
         return this.board[x][y];
     }
 
-    public Cell getElement(String x, String y){
+    public Cell getElement(String x, String y) {
         int xInt = xStringToInteger(x);
         int yInt = Integer.valueOf(y);
         return getElement(xInt, yInt);
-    }   
+    }
 
-    public static int xStringToInteger(String x){
+    public static int xStringToInteger(String x) {
         int LETTERS_IN_ALPHABET = 26;
 
-        if(x != x.toUpperCase()){
+        if (x != x.toUpperCase()) {
             return -1;
         }
 
         int position = 0;
-        for(int i = 0; i < x.length(); i++){
-           position += (int) x.charAt(i) - 65;
-           position += LETTERS_IN_ALPHABET * i;
+        for (int i = 0; i < x.length(); i++) {
+            position += (int) x.charAt(i) - 65;
+            position += LETTERS_IN_ALPHABET * i;
         }
 
         return position;
     }
 
-    public String printTable(){
+    public String printTable() {
         String result = "";
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
-                result += getElement(x, y).toString();
-                if(x != width -1){
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Cell cell = getElement(x, y);
+                if (cell == null) {
+                    result += "null";
+                } else {
+                    result += getElement(x, y).toString();
+                }
+                if (x != width - 1) {
                     result += " | ";
                 }
             }
