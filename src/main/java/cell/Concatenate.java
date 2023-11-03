9a7782608bfef273243ed8d73c2fdba7f9fd0aa4
magalhastudios1 <magalhastudios1@gmail.com>
2023-11-03 15:44:16 -0300

    Add Concatenate Cell

---
+++ src/main/java/cell/Concatenate.java
@@ -0,0 +1,37 @@
+package cell;
+
+import sheet.Sheet;
+
+public class Concatenate extends Cell implements TextFormula {
+
+    private int startX;
+    private int endX;
+
+    private int startY;
+    private int endY;
+
+    public Concatenate(int startX, int endX, int startY, int endY){
+        this.startX = startX;
+        this.endX = endX;
+
+        this.startY = startY;
+        this.endY = endY;
+    }
+
+    @Override
+    public String calculate(){
+        Sheet sheet = getSheet();
+        String result = "";
+        for(int x = startX; x < endX; x++){
+            for(int y = startY; y < endY; y++){
+                result += sheet.getElement(x, y).toString();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(this.calculate());
+    }
+}
