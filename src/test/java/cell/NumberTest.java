43ce5ef29fd02617522f50290365f37b0e17fa6f
magalhastudios1 <magalhastudios1@gmail.com>
2023-11-03 12:57:28 -0300

    Add more Number Cell tests

--- src/test/java/cell/NumberTest.java
+++ src/test/java/cell/NumberTest.java
@@ -7,7 +7,22 @@ import org.junit.Test;
 public class NumberTest {
 
   @Test
-  public void toString_integer() {
+  public void toString_integer_negative_floatStringWithTrace() {
+    assertEquals("-50.0", new Number(-50).toString());
+  }
+
+  @Test
+  public void toString_integer_positive_floatString() {
     assertEquals("50.0", new Number(50).toString());
+  }
+  
+  @Test
+  public void toString_float_negative_floatStringWithTrace() {
+    assertEquals("-50.0", new Number(-50.0).toString());
+  }
+
+  @Test
+  public void toString_float_positive_floatString() {
+    assertEquals("50.0", new Number(50.0).toString());
   }
 }
