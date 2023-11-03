6a2ec713c575f06ed7ca4daffd61505b8bd7397f
magalhastudios1 <magalhastudios1@gmail.com>
2023-11-03 14:35:01 -0300

    Add before in test

--- src/test/java/cell/SumTest.java
+++ src/test/java/cell/SumTest.java
@@ -2,11 +2,21 @@ package cell;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import sheet.Sheet;
 
 public class SumTest {
+    Sheet sheet;
+
+    @Before public void setUp() {
+       sheet = new Sheet(50, 50);
+    }
+
     @Test
     public void calculate_emptyInterval_returnsZero() {
         assertEquals(0.F, new Sum(0, 0, 0, 0).calculate(), 0);
@@ -14,9 +24,7 @@ public class SumTest {
 
     @Test
     public void calculate_oneElementInterval_returnsZero() {
-        Sheet sheet = new Sheet(50, 50);
         sheet.addElement(new Number(50), 0, 0);
-
         assertEquals(50.F, new Sum(0, 1, 0, 1).calculate(), 0);
     }
 }
