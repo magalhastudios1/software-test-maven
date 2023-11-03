9b5aa47fb79b920b75bf0d351bbacd66cd154398
magalhastudios1 <magalhastudios1@gmail.com>
2023-11-03 17:50:19 -0300

    Add Matrix Sum Cell and Test

---
+++ src/main/java/cell/MatrixSum.java
@@ -0,0 +1,47 @@
+package cell;
+
+import sheet.Sheet;
+
+public class MatrixSum extends Cell implements NumberFormula {
+    private int startAX;
+    private int startAY;
+
+    private int width;
+    private int height;
+
+    private int startBX;
+    private int startBY;
+
+    public MatrixSum(int startAX, int startAY, int width, int height, int startBX, int startBY) {
+        this.startAX = startAX;
+        this.startAY = startAY;
+
+        this.width = width;
+        this.height = height;
+
+        this.startBX = startBX;
+        this.startBY = startBY;
+    }
+
+    @Override
+    public float calculate() {
+        if (width == 0 || height == 0) {
+            return 0;
+        }
+        Sheet sheet = getSheet();
+        float result = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Number numberA = (Number) sheet.getElement(startAX + x, startAY + y);
+                Number numberB = (Number) sheet.getElement(startBX + x, startBY + y);
+                result += numberA.getValue() + numberB.getValue();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.calculate());
+    }
+}
---
+++ src/test/java/cell/MatrixSumTest.java
@@ -0,0 +1,61 @@
+package cell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sheet.Sheet;
+
+public class MatrixSumTest {
+    Sheet sheet;
+
+    @Before public void setUp() {
+       sheet = Sheet.getInstance(50, 50);
+    }
+
+    @Test
+    public void calculate_emptyInterval_returnsZero() {
+        assertEquals(0.F, new MatrixSum(0, 0, 0, 0, 0, 0).calculate(), 0);
+    }
+
+    @Test
+    public void calculate_oneElementIntervalIdentity_returnsDoubleElementValue() {
+        sheet.addElement(new Number(10), 0, 0);
+
+        assertEquals(20.F, new MatrixSum(0, 0, 1, 1, 0, 0).calculate(), 0);
+    }
+
+        @Test
+    public void calculate_oneElementInterval_returnsElementsSumValue() {
+        sheet.addElement(new Number(10), 0, 0);
+        sheet.addElement(new Number(20), 1, 0);
+        assertEquals(30.F, new MatrixSum(0, 0, 1, 1, 1, 0).calculate(), 0);
+    }
+
+    @Test
+    public void calculate_twoElementInterval_returnsElementsSumValue() {
+        sheet.addElement(new Number(10), 0, 0);
+        sheet.addElement(new Number(5), 1, 0);
+
+        sheet.addElement(new Number(20), 0, 1);
+        sheet.addElement(new Number(5), 1, 1);
+
+        assertEquals(40.F, new MatrixSum(0, 0, 2, 1, 0, 1).calculate(), 0);
+    }
+
+    @Test
+    public void calculate_squareMatrixElementInterval_returnsMatrixSumValue() {
+        sheet.addElement(new Number(1), 0, 0);
+        sheet.addElement(new Number(2), 1, 0);
+        sheet.addElement(new Number(3), 0, 1);
+        sheet.addElement(new Number(4), 1, 1);
+
+        sheet.addElement(new Number(1), 5, 5);
+        sheet.addElement(new Number(2), 6, 5);
+        sheet.addElement(new Number(3), 5, 6);
+        sheet.addElement(new Number(4), 6, 6);
+
+        assertEquals(20.F, new MatrixSum(0, 0, 2, 2, 5, 5).calculate(), 0);
+    }
+}
